eac24a83bfdad3c22b56cc467eead89a87d69cdd
shenpeng1 <shenpeng1@tal.com>
2020-11-16 10:59:58 +0800

    dev: 更新mac运行与连接方式

--- server/database/mysql.config.js
+++ server/database/mysql.config.js
@@ -3,10 +3,10 @@
  * 数据库配置文件
  */
 module.exports = {
-  host: '127.0.0.1',
+  host: 'localhost', // 安装mysql后需要更新localhost或127.0.0.1权限才能连接
   port: '3306',
   user: 'root',
-  password: 'root',
+  password: 'root1234',
   database: 'cms',
   timezone: '08:00' // 解决从数据库datetime类型取数据是 2019-09-30T12:53:51.000Z 问题
 }
